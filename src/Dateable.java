3ec1460ec39b7918c2b4fd3f3d5671a37598bee8
ic085 <ic0858bai@gmail.com>
2023-09-24 15:12:59 +0800

    Lab 1.1 complete

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
+        //java.lang.Object System;
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        System.out.println ("Question 1: Datability, Please enter your age:");
+        int n = scanner.nextInt();
+        System.out.print ("Dateable: " + age(n) + "\n");
     }
+    public static int age (int n) {return (n/2)+7;}
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
-public class Pages {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Pages {
+        public static void main(String[] args)
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println ("Question 2: Pages, Please enter your age: ");
+            int z= scanner.nextInt();
+            System.out.print ("Pages: " + page(z));
+        }
+        public static int page (int n) {return (100-n);}
 
     }
-}
+
